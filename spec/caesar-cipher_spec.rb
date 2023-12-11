8e6dbeec9e783a67aab8e482b56b17754c4045e3
ZarathustraSama <mr.gabriel@live.it>
2023-12-11 11:31:01 +0100

    Second test added

--- spec/caesar-cipher_spec.rb
+++ spec/caesar-cipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "shifts all lowercase letters to the correct value" do
     expect(caesar_cipher('hello', 3)).to eql('khoor')
   end
+
+  it "shifts all uppercase letters to the correct value" do
+    expect(caesar_cipher('HELLO', 3)).to eql('KHOOR')
+  end
 end
